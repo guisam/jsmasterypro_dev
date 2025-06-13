7e1102615d9e3028148a993c6dfbfd771f1e2d2e
Samson Gonzalez <sgonz@guidance.com>
2025-06-13 15:52:19 -0700

    Setup Fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@ const Home = () => {
   return (
     <div>
       <h1 className="h1-bold">Welcome to Next.js</h1>
-      <div className="card">This is a card on the page</div>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Next.js</h1>
+      <div className="font-inter">This is a card on the page</div>
     </div>
   );
 };
