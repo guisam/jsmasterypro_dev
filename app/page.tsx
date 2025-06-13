def4b49ca3a75ec017ec6f8662aed08ef4c33169
Samson Gonzalez <sgonz@guidance.com>
2025-06-13 15:18:33 -0700

    Finished Tailwind CSS Theming

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 const Home = () => {
   return (
     <div>
-      <h1 className="text-3xl text-light-500 font-black">Welcome to Next.js</h1>
+      <h1 className="h1-bold">Welcome to Next.js</h1>
+      <div className="card">This is a card on the page</div>
     </div>
   );
 };
