88512aa2e36ede5f4b409823e64b4bbaed75ff7b
Samson Gonzalez <sgonz@guidance.com>
2025-06-30 19:32:47 -0700

    Use refPath to bind the id to the type
    See: https://mongoosejs.com/docs/populate.html#dynamic-refpath

--- database/vote.model.ts
+++ database/vote.model.ts
@@ -3,17 +3,17 @@ import { model, models, Schema, Types } from "mongoose";
 export interface IVote {
   user: Types.ObjectId; // Reference to the User model
   id: Types.ObjectId; // This can be a Question or Answer ID
-  type: "question" | "answer"; // Specify the type of vote
+  type: "Question" | "Answer"; // Specify the type of vote
   voteType: "upvote" | "downvote"; // Type of vote
 }
 
 const VoteSchema = new Schema<IVote>(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-    id: { type: Schema.Types.ObjectId, required: true }, // This can be a Question or Answer ID
+    id: { type: Schema.Types.ObjectId, refPath: "type", required: true }, // This can be a Question or Answer ID
     type: {
       type: String,
-      enum: ["question", "answer"], // Specify the type of vote
+      enum: ["Question", "Answer"], // Specify the type of vote
       required: true,
     },
     voteType: {
